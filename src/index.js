5ba00c41ff83b09a1a8ad4909a911a923bbf4ce0
potato-lulw <om.patil17072@gmail.com>
2024-07-07 03:12:55 +0530

    Added utils to standardize the api errors and requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
 import dotenv from "dotenv"
 dotenv.config({ path: '.env' });
-import express from "express";
-const app = express();
+import app  from "./app.js";
 import { connectDB } from "./db/db.js";
 
 
